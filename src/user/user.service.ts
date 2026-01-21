98ef8dd7c99cde8085c0396f42f6674eb8454330
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-01-21 14:01:56 +0700

    update user service & auth

---
+++ src/user/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = any;
+
+@Injectable()
+export class UserService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'user1',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'user2',
+      password: 'guess',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
